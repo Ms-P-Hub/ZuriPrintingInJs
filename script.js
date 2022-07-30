24b2e85f406eccc35a5915b8051bc4e57633697d
Gugu Precious Mokwena <67442243+Ms-P-Hub@users.noreply.github.com>
2022-07-30 20:43:05 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("name").innerHTML = "Gugu Precious Mokwena"
+
+document.getElementById("length").innerHTML = "Length: I am an Average sized person (I don't know my height)"
+
+document.getElementById("country").innerHTML = "Country: South Africa"
